382c120db49379b71bf5489ee46395849783846f
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-09-05 00:21:14 +0300

    add new solved tasks

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -413,8 +413,22 @@ function getMatrixProduct(/* m1, m2 */) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+function evaluateTicTacToePosition(pos) {
+  for (let i = 0; i < 3; i += 1) {
+    if (pos[i][0] === pos[i][1] && pos[i][1] === pos[i][2] && pos[i][0] !== undefined) {
+      return pos[i][0];
+    }
+    if (pos[0][i] === pos[1][i] && pos[1][i] === pos[2][i] && pos[0][i] !== undefined) {
+      return pos[0][i];
+    }
+  }
+  if (pos[0][0] === pos[1][1] && pos[1][1] === pos[2][2] && pos[0][0] !== undefined) {
+    return pos[0][0];
+  }
+  if (pos[0][2] === pos[1][1] && pos[1][1] === pos[2][0] && pos[0][2] !== undefined) {
+    return pos[0][2];
+  }
+  return undefined;
 }
 
 
